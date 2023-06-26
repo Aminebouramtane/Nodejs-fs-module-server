7e8d3758492146efa8376da0121e5ca62d1ae58e
amine <bouramtaneamine1@gmail.com>
2023-06-26 12:37:26 +0100

    writefile

---
+++ fs.js
@@ -0,0 +1,10 @@
+const { log } = require("console");
+const fs  = require("fs");
+
+// fs.writeFile("amine.text","hey from fs file",(err,data)=>{
+//     if (err) {
+//         console.log(err);
+//     }else{
+//         console.log(data);
+//     }
+// })
