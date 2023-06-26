85a4b6ed5eac2e854d1bb5741732afd4289b5e2b
amine <bouramtaneamine1@gmail.com>
2023-06-26 12:41:53 +0100

    appendFile

--- fs.js
+++ fs.js
@@ -17,3 +17,10 @@ const fs  = require("fs");
 //         console.log(data);
 //     }
 // })
+
+fs.appendFile("./amine.text","This is my new content",(err)=>{
+    if (err) {
+        console.log(err);
+    }
+    console.log("data has been wrote succussfuly");
+})
